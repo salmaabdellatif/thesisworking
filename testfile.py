03a1a1a99893c2a6ed9974f4cb6c7575b624661f
G8 <salmaabdellatif@aucegypt.edu>
2023-09-25 15:41:08 +0300

    Your commit message here

---
+++ testfile.py
@@ -0,0 +1,11 @@
+# import tensorflow as tf
+# print(tf.__version__)
+# import librosa
+# print(librosa.__version__)
+# print(librosa.__file__)
+# import torch
+# torch.cuda.empty_cache()
+import torch
+# print(torch.__version__)
+torch.cuda.empty_cache()
+
